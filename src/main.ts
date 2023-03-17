407dc6d78c40b3688fcf4a36fe855990cdb770c2
李章源 <953251815@qq.com>
2023-03-17 15:07:00 +0800

    antdUI

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router).use(Antd)
 
 app.mount('#app')

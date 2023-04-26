792e479caf8dfbd407a938f836c5f8832d497353
杨丹婷 <1973142083@qq.com>
2023-04-26 16:55:03 +0800

    主页

--- src/components/Carousel/index.vue
+++ src/components/Carousel/index.vue
@@ -1,39 +1,41 @@
 <template>
-    <a-carousel :after-change="onChange">
+    <a-carousel autoplay>
       <a-carousel-slide>
-        <img src="your-image-url-1" alt="image-description-1" />
+        <img src="../../assets/1.jpg" alt="image-description-1" />
       </a-carousel-slide>
       <a-carousel-slide>
-        <img src="your-image-url-2" alt="image-description-2" />
+        <img src="../../assets/2.jpg" alt="image-description-2" />
       </a-carousel-slide>
       <a-carousel-slide>
-        <img src="your-image-url-3" alt="image-description-3" />
+        <img src="../../assets/3.jpg" alt="image-description-3" />
       </a-carousel-slide>
       <a-carousel-slide>
-        <img src="your-image-url-4" alt="image-description-4" />
+        <img src="../../assets/4.jpg" alt="image-description-4" />
       </a-carousel-slide>
     </a-carousel>
 </template>
 
 <script lang="ts" setup>
 
-const onChange = (current: number) => {
-      console.log(current);
-    };
+
 </script>
 
 <style scoped>
 
 .ant-carousel  {
   text-align: center;
-  height: 300px;
+  height: 350px;
   width:80%;
-  line-height: 300px;
+  line-height: 350px;
   background: #364d79;
   overflow: hidden;
 }
 
 .ant-carousel {
   color: #fff;
+}
+
+img{
+  width: 100%;
 }
 </style>
--- src/views/container/home/index.vue
+++ src/views/container/home/index.vue
@@ -2,15 +2,47 @@
     <div class="container">
         <myCarousel ></myCarousel>
         <div class="cardbox">
-            <a-card title="Card title" :bordered="true" style="width: 500px">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+            <a-card title="作业（待完成）" :bordered="true" style="width: 500px">
+                <a-list item-layout="horizontal" :data-source="data.homeWorkList">
+                  <template #renderItem="{ item }">
+                    <a-list-item>
+                        <template #actions>
+                          <a type="primary" @click="handleView(item)">查看</a>
+                        </template>
+                      <a-list-item-meta
+                        :description="item.content"
+                      >
+                        <template #title>
+                          <a href="https://www.antdv.com/">{{ item.title }}</a>
+                        </template>
+                        <template #avatar>
+                          <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                        </template>
+                      </a-list-item-meta>
+                    </a-list-item>
+                  </template>
+                </a-list>
             </a-card>
-            <a-card title="Card title" :bordered="true" style="width: 500px">
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+            <a-card title="近期消息" :bordered="true" style="width: 500px">
+                <a-list item-layout="horizontal" :data-source="data.messageArr">
+                  <template #renderItem="{ item }">
+                    <a-list-item>
+                        <template #actions>
+                          <a type="primary" @click="handleView(item)">查看</a>
+                        </template>
+                      <a-list-item-meta
+                        :description="item.content"
+                      >
+                        <template #title>
+                          <a href="https://www.antdv.com/">{{ item.theme }}</a>
+                        </template>
+                        <template #avatar>
+                          <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                        </template>
+                      </a-list-item-meta>
+                    </a-list-item>
+                  </template>
+                </a-list>
             </a-card>
         </div>
     </div>
@@ -18,19 +50,61 @@
 
 <script lang="ts" setup>
 import myCarousel from "@/components/Carousel/index.vue"
+import { getHomework, getMessage } from '@/network/home.js'
+import { ref, reactive, inject } from 'vue';
 
+const data =reactive({
+        messageArr : [{
+            id :'',
+            theme: '',
+            content :'',
+            publishTime : '',
+            state :''
+        }],
+        homeWorkList:[{
+            id :'',
+            title: '',
+            content :'',
+            startTime : '',
+            endTime :'',
+            completionStatus : ''
+        }]
+})
+
+getHomework()
+    .then((res: any) => {
+        if(res.data.code === 200){
+            data.homeWorkList = res.data.data
+        }
+    })
+getMessage()
+    .then((res:any) => {
+        if(res.data.code === 200){
+            data.messageArr = res.data.data
+        }
+    })
+function handleView (item) {
+    console.log('查看', item)
+    // 对列表项数据进行对应操作
+  }
 </script>
 
 <style lang="scss" scoped>
 .container{
     width: 100%;
     height: 100%;
-    display: grid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+    justify-content: flex-start;
 }
 
 .cardbox{
+    width: 80%;
+    padding-top: 30px;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    justify-content: space-between;
 }
 </style>
